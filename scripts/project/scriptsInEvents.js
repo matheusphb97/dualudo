6f7860e7e1cbfb3d5f0acd116bf4fe289baf83eb
DuaLudo <79771141+matheusphb97@users.noreply.github.com>
2024-11-24 23:44:12 -0300

    Add files via upload

---
+++ scripts/project/scriptsInEvents.js
@@ -0,0 +1,29 @@
+
+
+
+const scriptsInEvents = {
+
+	async Es_common_Event20_Act7(runtime, localVars)
+	{
+		localStorage.setItem("isMusic", runtime.globalVars.isMusic)
+	},
+
+	async Es_common_Event23_Act8(runtime, localVars)
+	{
+		localStorage.setItem("isSFX", runtime.globalVars.isSFX)
+	},
+
+	async Es_common_Event30_Act1(runtime, localVars)
+	{
+		const sfx = localStorage.getItem("isSFX");
+		const music = localStorage.getItem("isMusic") 
+		
+		runtime.globalVars.isSFX = (sfx == null ? 1:sfx);
+		
+		runtime.globalVars.isMusic = (music == null ? 1:music);
+	}
+
+};
+
+self.C3.ScriptsInEvents = scriptsInEvents;
+
